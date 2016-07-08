8a9b8990c411059fbd4a2c8cb670cc203457c3fe
butlerx <cianbutlerx@gmail.com>
2016-07-08 07:57:44 +0100

    update of css

--- themes/tranquilpeak/source/assets/js/script.js
+++ themes/tranquilpeak/source/assets/js/script.js
@@ -12468,6 +12468,12 @@ return jQuery;
     return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
   };
 })(jQuery, 'smartresize');
+;$(document).ready(function(){
+    $('#menu-buttons menu-button').each(function(index) {
+        if(this.href.trim() == window.location)
+            $(this).addClass("active");
+    });
+});
 ;(function($) {
   'use strict';
 
