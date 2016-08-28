8cbb35116f1f2d037a5a33f66ac954c3f93c9e74
butlerx <cianbutlerx@gmail.com>
2016-08-29 00:20:17 +0100

    add native 404 and 500

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var nodemailer = require("nodemailer");
-var app = express();
-var config = require("./config.json");
+var express = require('express'),
+nodemailer = require("nodemailer"),
+favicon = require('serve-favicon'),
+app = express(),
+config = require("./config.json");
 
 var smtpTransport = nodemailer.createTransport(config);
 var baseUrl = '/api/';
 
 app.use(express.static('public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/about/contact/*',function(req,res) {
-	var options = {
-		root: __dirname + '/public/',
-		dotfiles: 'deny',
-		headers: {
-			'x-timestamp': Date.now(),
-			'x-sent': true
-		}
-	};
+  var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
   var fileName = 'about/contact/contact.html';
-	console.log(fileName);
   res.sendFile(fileName, options, function (err) {
     if (err) {
       console.log(err);
@@ -31,12 +32,14 @@ app.get('/about/contact/*',function(req,res) {
 });
 
 app.get(baseUrl + 'contact', function(req, res) {
+	console.log(req.query);
+	to = req.query.to
   var mailOptions = {
     from: req.query.name + ' <' + req.query.email + '>',
-    to: req.query.to + '@redbrick.dcu.ie',
+    to: to + '@redbrick.dcu.ie',
     subject: '[Sent from the website] ' + req.query.subject,
     text : req.query.text,
-		replyTo: req.query.email
+    replyTo: req.query.email
   }
 
   console.log(mailOptions);
@@ -49,6 +52,18 @@ app.get(baseUrl + 'contact', function(req, res) {
       res.end("sent");
     }
   });
+});
+
+// Handle 404
+app.use(function(req, res) {
+    res.status(400);
+    res.render('404.jade', {title: '404: File Not Found'});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+    res.status(500);
+    res.render('500.jade', {title:'500: Internal Server Error', error: error});
 });
 
 app.listen(3000, function() {
