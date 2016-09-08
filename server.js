60c227c69fb1d3eb42fa7b1ee6d949de04ed5757
Cian Butler <butlerx@notthe.cloud>
2016-09-08 20:58:33 +0100

    tidy up server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express'),
 nodemailer = require("nodemailer"),
 favicon = require('serve-favicon'),
+config = require("./config.json"),
 app = express(),
-config = require("./config.json");
+smtpTransport = nodemailer.createTransport(config.email),
+baseUrl = '/api/';
 
-var smtpTransport = nodemailer.createTransport(config);
-var baseUrl = '/api/';
-
+// Serve Static files generate from hexo
+// TODO Freshly generate files with hexo on start
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+// dynamically create contact page
 app.get('/about/contact/*',function(req,res) {
   var options = {
     root: __dirname + '/public/',
@@ -31,13 +33,13 @@ app.get('/about/contact/*',function(req,res) {
   });
 });
 
+// contact form email sender
 app.get(baseUrl + 'contact', function(req, res) {
-
-  recaptcha.verify(function(success, error_code) {
+  recaptcha.verify( function (success, error_code) {
     if (success) {
       res.send('Recaptcha response valid.');
     } else {
-      to = req.query.to
+      to = req.query.to;
       var mailOptions = {
         from: req.query.name + ' <' + req.query.email + '>',
         to: to + '@redbrick.dcu.ie',
@@ -45,9 +47,8 @@ app.get(baseUrl + 'contact', function(req, res) {
         text : req.query.text,
         replyTo: req.query.email
       }
-
       smtpTransport.sendMail(mailOptions, function (error, info) {
-        if(error) {
+        if (error) {
           console.log(error);
           res.end("error");
         } else {
@@ -60,14 +61,16 @@ app.get(baseUrl + 'contact', function(req, res) {
 });
 
 // Handle 404
-app.use(function(req, res) {
-    res.status(404).sendFile(__dirname + '/public/404.html');
+app.use( function(req, res) {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+  console.log('404', req);
 });
 
 // Handle 500
-app.use(function(error, req, res, next) {
-    res.status(500);
-    res.render('500.jade', {title:'500: Internal Server Error', error: error});
+app.use( function(error, req, res, next) {
+  res.status(500);
+  res.render('500.jade', {title:'500: Internal Server Error', error: error});
+  console.log('500', req);
 });
 
 app.listen(3000, function() {
