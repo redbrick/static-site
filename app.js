4e350e4fc30e60dded6ca6bf42270879d8824774
butlerx <cianbutlerx@gmail.com>
2016-09-25 10:04:23 +0100

    more detailed logs and linting

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-var express = require('express'),
-nodemailer = require("nodemailer"),
-path = require('path'),
-favicon = require('serve-favicon'),
-logger = require('morgan'),
-cookieParser = require('cookie-parser'),
-bodyParser = require('body-parser');
-config = require("./config.json"),
-app = express(),
-smtpTransport = nodemailer.createTransport(config.email),
-baseUrl = '/api/';
+var express = require('express');
+var nodemailer = require('nodemailer');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ReCAPTCHA = require('recaptcha2');
 
-// Serve Static files generate from hexo
-// TODO Freshly generate files with hexo on start
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+var app = express();
+
+var config = require('./config.json');
+var recaptcha = new ReCAPTCHA({
+  siteKey: config.recaptcha.siteKey,
+  secretKey: config.repatcha.secretKey
+});
+var smtpTransport = nodemailer.createTransport(config.email);
+var baseUrl = '/api/';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Serve Static files generate from hexo
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // dynamically create contact page
-app.get('/about/contact/*',function(req,res) {
+app.get('/about/contact/*', function (req, res) {
   var options = {
-    root: __dirname + '/public/',
+    root: path.join(__dirname, '/public/'),
     dotfiles: 'deny',
     headers: {
       'x-timestamp': Date.now(),
@@ -38,31 +44,28 @@ app.get('/about/contact/*',function(req,res) {
       console.log(err);
       res.status(err.status).end();
     }
-    else {
-      console.log('Sent:', fileName);
-    }
   });
 });
 
 // contact form email sender
-app.get(baseUrl + 'contact', function(req, res) {
-  recaptcha.verify( function (success, error_code) {
-    if (success) {
-      res.send('Recaptcha response valid.');
+app.get(baseUrl + 'contact', function (req, res) {
+  recaptcha.verify(function (success, error) {
+    if (!success) {
+      res.send('Recaptcha response invalid.');
     } else {
-      to = req.query.to;
+      var to = req.query.to;
       var mailOptions = {
         from: req.query.name + ' <' + req.query.email + '>',
         to: to + '@redbrick.dcu.ie',
         subject: '[Sent from the website] ' + req.query.subject,
-        text : req.query.text,
+        text: req.query.text,
         replyTo: req.query.email
-      }
+      };
       smtpTransport.sendMail(mailOptions, function (error, info) {
         if (error) {
-          res.end("error");
+          res.end('error');
         } else {
-          res.end("sent");
+          res.end('sent');
         }
       });
     }
@@ -70,17 +73,17 @@ app.get(baseUrl + 'contact', function(req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  res.status(err.status).sendFile(path.join(__dirname + '/public/404.html'));
+  res.status(err.status).sendFile(path.join(__dirname, '/public/404.html'));
 });
 
 // error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -91,8 +94,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500).sendFile(path.join(__dirname + '/public/404.html'));
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).sendFile(path.join(__dirname, '/public/404.html'));
 });
 
 module.exports = app;
