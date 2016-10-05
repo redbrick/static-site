388273c436fc621c64ca423a91cf2494fe1b5377
Clement Allen <clementallen313@gmail.com>
2016-10-05 19:39:30 +0100

    Added logging to file and log rotation (#57)
    * Added logging to file and log rotation
    
    * Added winston logging
    
    * Changed to use logger.info and logger.error

---
+++ logger.js
@@ -0,0 +1,24 @@
+const FileStreamRotator = require('file-stream-rotator');
+const logger = require('winston');
+const yaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs');
+
+const configFile = fs.readFileSync('./_config.yml', 'utf8');
+const config = yaml.safeLoad(configFile).server;
+const logDirectory = path.join(__dirname, config.logDirectory);
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+var winstonLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'winston-%DATE%.log'),
+  frequency: config.logRotationFreqency,
+  verbose: false
+});
+
+logger.add(logger.transports.File, {
+  stream: winstonLogStream
+});
+
+module.exports = logger;
