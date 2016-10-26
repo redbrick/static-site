60263713c58fd6cb0dbde71705e989897d468c00
butlerx <cianbutlerx@gmail.com>
2016-10-26 15:28:32 +0100

    fix yaml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rsync -val -e 'ssh -i .ssh/id_zeus.redbrick.dcu.ie' * circle@zeus.redbrick.dcu.ie:/var/www/redbrick
+rsync -val -e 'ssh -i /home/ubuntu/.ssh/id_zeus.redbrick.dcu.ie' * circle@zeus.redbrick.dcu.ie:/var/www/redbrick
