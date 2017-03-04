6d9b6bb0a542722a6c4f32a9e3a6be1e881789dc
Cian Butler <butlerx@notthe.cloud>
2017-03-04 09:56:21 +0000

    switch linter to eslint and fix errors (#135)

--- routes/contactForm.js
+++ routes/contactForm.js
@@ -1,21 +1,20 @@
-'use strict';
 const express = require('express');
 const path = require('path');
 const router = express.Router();
 const logger = require('../lib/logger');
 
 // dynamically create contact page
-router.get('*', function (req, res) {
-  let options = {
-    root: path.join(__dirname, '..', '/public/'),
+router.get('*', (req, res) => {
+  const options = {
+    root    : path.join(__dirname, '..', '/public/'),
     dotfiles: 'deny',
-    headers: {
+    headers : {
       'x-timestamp': Date.now(),
-      'x-sent': true
-    }
+      'x-sent'     : true,
+    },
   };
   const fileName = 'about/contact/contact.html';
-  res.sendFile(fileName, options, function (err) {
+  res.sendFile(fileName, options, err => {
     if (err) {
       logger.error(err);
       res.status(err.status).end();
