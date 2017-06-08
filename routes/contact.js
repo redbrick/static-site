470c5339db4bbc91af8a353a8736e56b0cf611d1
Cian Butler <butlerx@notthe.cloud>
2017-06-08 17:05:17 +0100

    make smtp more configurable (#200)

--- routes/contact.js
+++ routes/contact.js
@@ -9,6 +9,7 @@ const sendMailTransport = require('../lib/sendMailTransport');
 
 const configFile = fs.readFileSync('./_config.yml', 'utf8');
 const config = yaml.safeLoad(configFile).server;
+const sendMailDefault = true;
 const recaptcha = new ReCAPTCHA({
   siteKey  : process.env.RECAPTCHA_SITE_KEY || config.recaptcha.siteKey,
   secretKey: process.env.RECAPTCHA_SECRET_KEY || config.recaptcha.secretKey,
@@ -28,28 +29,31 @@ router.get('/contact', ({ query }, res) => {
         text   : query.text,
         replyTo: query.email,
       };
-      smtpTransport.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          logger.error(error);
-          sendMailTransport.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              logger.error(error);
-              res.json({ formSubmit: true, errors: 'error in SMTP Server' });
-            } else {
-              logger.info(info);
-              res.json({ formSubmit: true, errors: null });
-            }
-          });
-        } else {
-          logger.info(info);
-          res.json({ formSubmit: true, errors: null });
-        }
-      });
+      if (!config.useSendMail || sendMailDefault) {
+        smtpTransport.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            logger.error(`error in smtp server ${error}`);
+            res.json({ formSubmit: true, errors: 'error in SMTP Server' });
+          } else {
+            logger.info(info);
+            res.json({ formSubmit: true, errors: null });
+          }
+        });
+      } else {
+        sendMailTransport.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            logger.error(`error in SendMail ${error}`);
+            res.json({ formSubmit: true, errors: 'error in SendMail' });
+          } else {
+            logger.info(info);
+            res.json({ formSubmit: true, errors: null });
+          }
+        });
+      }
     })
     .catch(errorCodes => {
       // invalid
-      console.log('error in captcha', errorCodes);
-      logger.error(errorCodes);
+      logger.error(`error in captcha ${errorCodes}`);
       res.json({ formSubmit: false, errors: recaptcha.translateErrors(errorCodes) });
     });
 });
